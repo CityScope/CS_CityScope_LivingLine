9a0ac3f1fdfb2d723ff2248ee6b67f846d04c54b
Tianyu Su <sutianyu@mit.edu>
2019-03-18 13:39:54 -0400

    Import Radar Chart Package
    20190318
    Tianyu
    
    Import the Radar Chart Package from City Matrix.

---
+++ Assets/Scripts/AI UI/AI Radar/CityMatrixRadarChart.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CityMatrixRadarChart : MonoBehaviour {
+
+    // shared
+    public GameObject centerRefPt;
+    public GameObject[] endRefPts;
+
+    // current
+    public GameObject[] movingRefPts;
+    [Range(0, 1f)]
+    public float[] metrics;
+    public GameObject currentLineDensityDiversity;
+    public GameObject currentLineEnergyTrafficSolar;
+    public GameObject currentLineAll;
+    public GameObject currentDashLineDiversityEnergy;
+    public GameObject currentDashLineSolarDensity;
+    public GameObject currentFillDensityDiversity;
+    public GameObject currentFillEnergyTraffic;
+    public GameObject currentFillTrafficSolar;
+    public GameObject currentFillDiversityEnergy;
+    public GameObject currentFillSolarDensity;
+
+    //private float[] prev = new float[9];
+    private Vector3[] movingPts = new Vector3[9];
+
+            // CURRENT
+            // update moving ref pts' locations
+            for (int i = 0; i < metrics.Length; i++)
+            {
+                Vector3 start = centerRefPt.transform.position;
+                Vector3 end = endRefPts[i].transform.position;
+                Vector3 v = (end - start) * metrics[i];
+                Vector3 p = start + v;
+                movingPts[i] = p;
+                // moving ref pt gameobjects for fill mesh
+                movingRefPts[i].transform.position = p;
+            }
+
+            // update lines
+            //currentLineDensityDiversity.GetComponent<LineRenderer>().SetPosition(0, movingRefPts[0].transform.position);
+            //currentLineDensityDiversity.GetComponent<LineRenderer>().SetPosition(1, movingPts[1]);
+            //currentLineEnergyTrafficSolar.GetComponent<LineRenderer>().SetPosition(0, movingPts[2]);
+            //currentLineEnergyTrafficSolar.GetComponent<LineRenderer>().SetPosition(1, movingPts[3]);
+            //currentLineEnergyTrafficSolar.GetComponent<LineRenderer>().SetPosition(2, movingPts[4]);
+            currentLineAll.GetComponent<LineRenderer>().SetPosition(0, movingPts[0]);
+            currentLineAll.GetComponent<LineRenderer>().SetPosition(1, movingPts[1]);
+            currentLineAll.GetComponent<LineRenderer>().SetPosition(2, movingPts[2]);
+            currentLineAll.GetComponent<LineRenderer>().SetPosition(3, movingPts[3]);
+            currentLineAll.GetComponent<LineRenderer>().SetPosition(4, movingPts[4]);
+            currentLineAll.GetComponent<LineRenderer>().SetPosition(5, movingPts[0]);
+
+}
---
+++ Assets/Scripts/AI UI/AI Radar/HighLevelScoreCtrl.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class HighLevelScoreCtrl : MonoBehaviour
+{
+    //public GameObject orange;
+    public CityMatrixRadarChart CMRadarChart;
+
+    public Text innovationPotentialScore;
+    public Text resourceEfficiencyScore;
+    public Text totalScore;
+    public Text innovationPotentialScoreSuggested;
+    public Text resourceEfficiencyScoreSuggested;
+    public Text totalScoreSuggested;
+    public GameObject currentScores;
+    public GameObject suggestedScores;
+    public GameObject arrows;
+    public bool showAISuggestion;
+    public Slider balance;
+    public Text balanceText1;
+    public Text balanceText2;
+
+    void Update()
+    {
+        // visibility control
+        if (this.showAISuggestion)
+        {
+            //currentScores.transform.localPosition = new Vector3(-40.0f, 0.0f, 0.0f);
+            suggestedScores.SetActive(true);
+            //arrows.SetActive(true);
+        }
+        else
+        {
+            //currentScores.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+            suggestedScores.SetActive(false);
+            //arrows.SetActive(false);
+        }
+
+        // current
+        float IPScore = (CMRadarChart.metrics[0] + CMRadarChart.metrics[1]) / 2.0f * 100.0f;
+        innovationPotentialScore.text = string.Format("{0:0}", IPScore);
+
+        float REScore = (CMRadarChart.metrics[2] + CMRadarChart.metrics[3]
+            + CMRadarChart.metrics[4]) / 3.0f * 100.0f;
+        resourceEfficiencyScore.text = string.Format("{0:0}", REScore);
+
+        float TScore = IPScore * (1 - balance.value) + REScore * balance.value;
+        totalScore.text = string.Format("{0:0}", TScore);
+
+        balanceText1.text = string.Format("{0:0}", (1 - balance.value) * 100.0f) + "%";
+        balanceText2.text = string.Format("{0:0}", balance.value * 100.0f) + "%";
+
+        // suggested
+        float IPScoreSuggested = (CMRadarChart.metricsSuggested[0] + CMRadarChart.metricsSuggested[1]) / 2.0f * 100.0f;
+        float IPDelta = IPScoreSuggested - IPScore;
+        string IPDeltaStr;
+        if (IPDelta >= 0.0f)
+        {
+            IPDeltaStr = "+" + string.Format("{0:0}", IPDelta);
+        }
+        else
+        {
+            IPDeltaStr = "-" + string.Format("{0:0}", -IPDelta);
+        }
+        innovationPotentialScoreSuggested.text = IPDeltaStr;
+
+        float REScoreSuggested = (CMRadarChart.metricsSuggested[2] + CMRadarChart.metricsSuggested[3]
+            + CMRadarChart.metricsSuggested[4]) / 3.0f * 100.0f;
+        float REDelta = REScoreSuggested - REScore;
+        string REDeltaStr;
+        if (REDelta >= 0.0f)
+        {
+            REDeltaStr = "+" + string.Format("{0:0}", REDelta);
+        }
+        else
+        {
+            REDeltaStr = "-" + string.Format("{0:0}", -REDelta);
+        }
+        resourceEfficiencyScoreSuggested.text = REDeltaStr;
+
+
+        float TSuggested = IPScoreSuggested * (1 - balance.value) + REScoreSuggested * balance.value;
+        float TDelta = TSuggested - TScore;
+        string TDeltaStr;
+        if (TDelta >= 0.0f)
+        {
+            TDeltaStr = "+" + string.Format("{0:0}", TDelta);
+        }
+        else
+        {
+            TDeltaStr = "-" + string.Format("{0:0}", -TDelta);
+        }
+        totalScoreSuggested.text = TDeltaStr;
+    }
+}

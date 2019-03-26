602b04fc0a57bce7c18307ff2db1ddfb40082a00
Tianyu Su <sutianyu@mit.edu>
2019-03-26 15:22:54 -0400

    prefab rotate

--- Assets/Scripts/RotateController.cs
+++ Assets/Scripts/RotateController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour
+public class RotateController : MonoBehaviour
 {
     // Update is called once per frame
     private void Update()

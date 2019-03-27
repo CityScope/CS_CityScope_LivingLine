cfc51b79c8e670a186ce70686957e54aeb19acd8
Ryan Yan Zhang <ryanz@mit.edu>
2019-03-27 00:52:50 -0400

    Import unitypackage from cooper hewitt 3D projection mapping project by Ryan Zhang and intergrate to current project
    https://github.com/popabczhang/3d_projection_mapping_unity

---
+++ Assets/Scripts/BoxMorphKeyPts.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMorphKeyPts : MonoBehaviour {
+
+    public bool calibrating = false;
+    public GameObject keyPts;
+
+    public GameObject[] OriginBoxKeyPts;
+    public GameObject[] TargetBoxKeyPts;
+    public Vector3 a1, b1, c1, d1, e1, f1, g1, h1, a2, b2, c2, d2, e2, f2, g2, h2;
+
+    // Use this for initialization
+    void Start () {
+
+        a1 = OriginBoxKeyPts[0].transform.position;
+        b1 = OriginBoxKeyPts[1].transform.position;
+        c1 = OriginBoxKeyPts[2].transform.position;
+        d1 = OriginBoxKeyPts[3].transform.position;
+        e1 = OriginBoxKeyPts[4].transform.position;
+        f1 = OriginBoxKeyPts[5].transform.position;
+        g1 = OriginBoxKeyPts[6].transform.position;
+        h1 = OriginBoxKeyPts[7].transform.position;
+
+        a2 = TargetBoxKeyPts[0].transform.position;
+        b2 = TargetBoxKeyPts[1].transform.position;
+        c2 = TargetBoxKeyPts[2].transform.position;
+        d2 = TargetBoxKeyPts[3].transform.position;
+        e2 = TargetBoxKeyPts[4].transform.position;
+        f2 = TargetBoxKeyPts[5].transform.position;
+        g2 = TargetBoxKeyPts[6].transform.position;
+        h2 = TargetBoxKeyPts[7].transform.position;
+
+        // if there's some Box Morph data saved already
+        //Debug.Log("a2x" + gameObject.name);
+        if (PlayerPrefs.GetFloat("a2x" + gameObject.name, 9999f) != 9999f)
+        {
+            LoadBoxMorph();
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (calibrating)
+        {
+
+            a1 = OriginBoxKeyPts[0].transform.position;
+            b1 = OriginBoxKeyPts[1].transform.position;
+            c1 = OriginBoxKeyPts[2].transform.position;
+            d1 = OriginBoxKeyPts[3].transform.position;
+            e1 = OriginBoxKeyPts[4].transform.position;
+            f1 = OriginBoxKeyPts[5].transform.position;
+            g1 = OriginBoxKeyPts[6].transform.position;
+            h1 = OriginBoxKeyPts[7].transform.position;
+
+            a2 = TargetBoxKeyPts[0].transform.position;
+            b2 = TargetBoxKeyPts[1].transform.position;
+            c2 = TargetBoxKeyPts[2].transform.position;
+            d2 = TargetBoxKeyPts[3].transform.position;
+            e2 = TargetBoxKeyPts[4].transform.position;
+            f2 = TargetBoxKeyPts[5].transform.position;
+            g2 = TargetBoxKeyPts[6].transform.position;
+            h2 = TargetBoxKeyPts[7].transform.position;
+
+        }
+		
+	}
+
+    public void ToggleCalibration(bool toggleValue)
+    {
+        calibrating = toggleValue;
+        keyPts.SetActive(toggleValue);
+    }
+
+    public void SaveBoxMorph()
+    {
+        PlayerPrefs.SetFloat("a2x" + gameObject.name, a2.x);
+        PlayerPrefs.SetFloat("a2z" + gameObject.name, a2.z);
+        PlayerPrefs.SetFloat("b2x" + gameObject.name, b2.x);
+        PlayerPrefs.SetFloat("b2z" + gameObject.name, b2.z);
+        PlayerPrefs.SetFloat("c2x" + gameObject.name, c2.x);
+        PlayerPrefs.SetFloat("c2z" + gameObject.name, c2.z);
+        PlayerPrefs.SetFloat("d2x" + gameObject.name, d2.x);
+        PlayerPrefs.SetFloat("d2z" + gameObject.name, d2.z);
+        PlayerPrefs.SetFloat("e2x" + gameObject.name, e2.x);
+        PlayerPrefs.SetFloat("e2z" + gameObject.name, e2.z);
+        PlayerPrefs.SetFloat("f2x" + gameObject.name, f2.x);
+        PlayerPrefs.SetFloat("f2z" + gameObject.name, f2.z);
+        PlayerPrefs.SetFloat("g2x" + gameObject.name, g2.x);
+        PlayerPrefs.SetFloat("g2z" + gameObject.name, g2.z);
+        PlayerPrefs.SetFloat("h2x" + gameObject.name, h2.x);
+        PlayerPrefs.SetFloat("h2z" + gameObject.name, h2.z);
+    }
+
+    public void LoadBoxMorph()
+    {
+        a2 = new Vector3(PlayerPrefs.GetFloat("a2x" + gameObject.name, a2.x), a2.y, PlayerPrefs.GetFloat("a2z" + gameObject.name, a2.z));
+        b2 = new Vector3(PlayerPrefs.GetFloat("b2x" + gameObject.name, b2.x), b2.y, PlayerPrefs.GetFloat("b2z" + gameObject.name, b2.z));
+        c2 = new Vector3(PlayerPrefs.GetFloat("c2x" + gameObject.name, c2.x), c2.y, PlayerPrefs.GetFloat("c2z" + gameObject.name, c2.z));
+        d2 = new Vector3(PlayerPrefs.GetFloat("d2x" + gameObject.name, d2.x), d2.y, PlayerPrefs.GetFloat("d2z" + gameObject.name, d2.z));
+        e2 = new Vector3(PlayerPrefs.GetFloat("e2x" + gameObject.name, e2.x), e2.y, PlayerPrefs.GetFloat("e2z" + gameObject.name, e2.z));
+        f2 = new Vector3(PlayerPrefs.GetFloat("f2x" + gameObject.name, f2.x), f2.y, PlayerPrefs.GetFloat("f2z" + gameObject.name, f2.z));
+        g2 = new Vector3(PlayerPrefs.GetFloat("g2x" + gameObject.name, g2.x), g2.y, PlayerPrefs.GetFloat("g2z" + gameObject.name, g2.z));
+        h2 = new Vector3(PlayerPrefs.GetFloat("h2x" + gameObject.name, h2.x), h2.y, PlayerPrefs.GetFloat("h2z" + gameObject.name, h2.z));
+
+        TargetBoxKeyPts[0].transform.position = a2;
+        TargetBoxKeyPts[1].transform.position = b2;
+        TargetBoxKeyPts[2].transform.position = c2;
+        TargetBoxKeyPts[3].transform.position = d2;
+        TargetBoxKeyPts[4].transform.position = e2;
+        TargetBoxKeyPts[5].transform.position = f2;
+        TargetBoxKeyPts[6].transform.position = g2;
+        TargetBoxKeyPts[7].transform.position = h2;
+    }
+
+    public void ResetBoxMorph()
+    {
+        a2 = OriginBoxKeyPts[0].transform.position;
+        b2 = OriginBoxKeyPts[1].transform.position;
+        c2 = OriginBoxKeyPts[2].transform.position;
+        d2 = OriginBoxKeyPts[3].transform.position;
+        e2 = OriginBoxKeyPts[4].transform.position;
+        f2 = OriginBoxKeyPts[5].transform.position;
+        g2 = OriginBoxKeyPts[6].transform.position;
+        h2 = OriginBoxKeyPts[7].transform.position;
+
+        TargetBoxKeyPts[0].transform.position = a2;
+        TargetBoxKeyPts[1].transform.position = b2;
+        TargetBoxKeyPts[2].transform.position = c2;
+        TargetBoxKeyPts[3].transform.position = d2;
+        TargetBoxKeyPts[4].transform.position = e2;
+        TargetBoxKeyPts[5].transform.position = f2;
+        TargetBoxKeyPts[6].transform.position = g2;
+        TargetBoxKeyPts[7].transform.position = h2;
+    }
+
+
+}
---
+++ Assets/Scripts/CamControlByUI.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamControlByUI : MonoBehaviour {
+
+    public GameObject cam;
+    public float speedPos;
+    public float speedRot;
+    public float speedFov;
+
+    public Toggle toggleUI;
+    public GameObject camInputMenu;
+    public Text textA;
+    public Text textB;
+    public Text textC;
+    public Text textD;
+    public Text textE;
+    public Text textF;
+
+    void Start () {
+        cam = gameObject;
+
+        SaveReset();
+        
+        // if there's some cam data saved already
+        if (PlayerPrefs.GetFloat("camPosX", 9999f) != 9999f)
+        {
+            Load();
+        }
+    }
+
+    private void Update()
+    {
+        if (toggleUI.enabled && camInputMenu.activeInHierarchy)
+        {
+            textA.text = string.Format("a = {0}", Mathf.Round(cam.transform.localPosition.x*10000f)/10000f);
+            textB.text = string.Format("b = {0}", Mathf.Round((cam.transform.localPosition.y-0.778f) * 10000f) / 10000f);
+            textC.text = string.Format("c = {0}", Mathf.Round(cam.transform.localPosition.z * 10000f) / 10000f);
+            textD.text = string.Format("d = {0}", Mathf.Round(-(cam.transform.localEulerAngles.x - 90f) * 10000f) / 10000f);
+            textE.text = string.Format("e = {0}", Mathf.Round(cam.transform.localRotation.eulerAngles.z * 10000f) / 10000f);
+            textF.text = string.Format("f = {0}", Mathf.Round(cam.GetComponent<Camera>().fieldOfView * 10000f) / 10000f);
+        }
+    }
+
+    public void CamLeft()
+    {
+        cam.transform.Translate(new Vector3(-speedPos, 0f, 0f), Space.Self);
+    }
+
+    public void CamRight()
+    {
+        cam.transform.Translate(new Vector3(speedPos, 0f, 0f), Space.Self);
+    }
+
+    public void CamUp()
+    {
+        cam.transform.Translate(new Vector3(0f, speedPos, 0f), Space.Self);
+    }
+
+    public void CamDown()
+    {
+        cam.transform.Translate(new Vector3(0f, -speedPos, 0f), Space.Self);
+    }
+
+    public void CamBackward()
+    {
+        cam.transform.Translate(new Vector3(0f, 0f, -speedPos), Space.Self);
+    }
+
+    public void CamForward()
+    {
+        cam.transform.Translate(new Vector3(0f, 0f, speedPos), Space.Self);
+    }
+
+    public void CamCW()
+    {
+        cam.transform.Rotate(cam.transform.up, speedRot, Space.Self);
+    }
+
+    public void CamCCW()
+    {
+        cam.transform.Rotate(cam.transform.up, -speedRot, Space.Self);
+    }
+
+    public void CamNodUp()
+    {
+        cam.transform.Rotate(cam.transform.right, -speedRot, Space.Self);
+    }
+
+    public void CamNodDown()
+    {
+        cam.transform.Rotate(cam.transform.right, speedRot, Space.Self);
+    }
+
+    public void CamNarrow()
+    {
+        float fov = cam.GetComponent<Camera>().fieldOfView;
+        cam.GetComponent<Camera>().fieldOfView = fov - speedFov;
+    }
+
+    public void CamWide()
+    {
+        float fov = cam.GetComponent<Camera>().fieldOfView;
+        cam.GetComponent<Camera>().fieldOfView = fov + speedFov;
+    }
+
+    public void CamSpeedUp()
+    {
+        speedPos = speedPos * 2.0f;
+        speedRot = speedRot * 2.0f;
+        speedFov = speedFov * 2.0f;
+    }
+
+    public void CamSpeedDown()
+    {
+        speedPos = speedPos / 2.0f;
+        speedRot = speedRot / 2.0f;
+        speedFov = speedFov / 2.0f;
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("camPosX", cam.transform.localPosition.x);
+        PlayerPrefs.SetFloat("camPosY", cam.transform.localPosition.y);
+        PlayerPrefs.SetFloat("camPosZ", cam.transform.localPosition.z);
+        PlayerPrefs.SetFloat("camRotX", cam.transform.localEulerAngles.x);
+        PlayerPrefs.SetFloat("camRotY", cam.transform.localEulerAngles.y);
+        PlayerPrefs.SetFloat("camRotZ", cam.transform.localEulerAngles.z);
+        PlayerPrefs.SetFloat("camFov", cam.GetComponent<Camera>().fieldOfView);
+    }
+
+    public void Load()
+    {
+        float px = PlayerPrefs.GetFloat("camPosX", cam.transform.localPosition.x);
+        float py = PlayerPrefs.GetFloat("camPosY", cam.transform.localPosition.y);
+        float pz = PlayerPrefs.GetFloat("camPosZ", cam.transform.localPosition.z);
+        cam.transform.localPosition = new Vector3(px, py, pz);
+        float rx = PlayerPrefs.GetFloat("camRotX", cam.transform.localEulerAngles.x);
+        float ry = PlayerPrefs.GetFloat("camRotY", cam.transform.localEulerAngles.y);
+        float rz = PlayerPrefs.GetFloat("camRotZ", cam.transform.localEulerAngles.z);
+        cam.transform.localEulerAngles = new Vector3(rx, ry, rz);
+        float fov = PlayerPrefs.GetFloat("camFov", cam.GetComponent<Camera>().fieldOfView);
+        cam.GetComponent<Camera>().fieldOfView = fov;
+    }
+
+    public void SaveReset()
+    {
+        PlayerPrefs.SetFloat("camResetPosX", cam.transform.localPosition.x);
+        PlayerPrefs.SetFloat("camResetPosY", cam.transform.localPosition.y);
+        PlayerPrefs.SetFloat("camResetPosZ", cam.transform.localPosition.z);
+        PlayerPrefs.SetFloat("camResetRotX", cam.transform.localEulerAngles.x);
+        PlayerPrefs.SetFloat("camResetRotY", cam.transform.localEulerAngles.y);
+        PlayerPrefs.SetFloat("camResetRotZ", cam.transform.localEulerAngles.z);
+        PlayerPrefs.SetFloat("camResetFov", cam.GetComponent<Camera>().fieldOfView);
+    }
+
+    public void LoadReset()
+    {
+        float px = PlayerPrefs.GetFloat("camResetPosX", cam.transform.localPosition.x);
+        float py = PlayerPrefs.GetFloat("camResetPosY", cam.transform.localPosition.y);
+        float pz = PlayerPrefs.GetFloat("camResetPosZ", cam.transform.localPosition.z);
+        cam.transform.localPosition = new Vector3(px, py, pz);
+        float rx = PlayerPrefs.GetFloat("camResetRotX", cam.transform.localEulerAngles.x);
+        float ry = PlayerPrefs.GetFloat("camResetRotY", cam.transform.localEulerAngles.y);
+        float rz = PlayerPrefs.GetFloat("camResetRotZ", cam.transform.localEulerAngles.z);
+        cam.transform.localEulerAngles = new Vector3(rx, ry, rz);
+        float fov = PlayerPrefs.GetFloat("camResetFov", cam.GetComponent<Camera>().fieldOfView);
+        cam.GetComponent<Camera>().fieldOfView = fov;
+    }
+
+    public void InputA(string value)
+    {
+        //Debug.Log(string.Format("value = {0}", value));
+        float a = float.Parse(value);
+        //Debug.Log(string.Format("a = {0}", a));
+        cam.transform.localPosition = new Vector3(a, cam.transform.localPosition.y, cam.transform.localPosition.z);
+    }
+
+    public void InputB(string value)
+    {
+        float b = float.Parse(value);
+        Debug.Log(string.Format("b = {0}", b));
+        cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, b+0.778f, cam.transform.localPosition.z);
+    }
+
+    public void InputC(string value)
+    {
+        float c = float.Parse(value);
+        cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, c);
+    }
+
+    public void InputD(string value)
+    {
+        float d = float.Parse(value);
+        cam.transform.localEulerAngles = new Vector3(90f-d, cam.transform.localEulerAngles.y, cam.transform.localEulerAngles.z);
+    }
+
+    public void InputE(string value)
+    {
+        float e = float.Parse(value);
+        cam.transform.localEulerAngles = new Vector3(cam.transform.localEulerAngles.x, cam.transform.localPosition.y, e);
+    }
+
+    public void InputF(string value)
+    {
+        float f = float.Parse(value);
+        cam.GetComponent<Camera>().fieldOfView = f;
+    }
+
+}

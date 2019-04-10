c280b858317b36a4e2c12fa4cc83e912ed88c327
Ryan Yan Zhang <ryanz@mit.edu>
2019-04-09 22:00:43 -0400

    Done: UI for calibrating 2 display
    the camera depth need to be changed for mouse drag to work on multiple display!

---
+++ Assets/Scripts/SetCamDepth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCamDepth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetDepth(bool toggleValue)
+    {
+        if (toggleValue)
+        {
+            GetComponent<Camera>().depth = 0;
+        }
+        else
+        {
+            GetComponent<Camera>().depth = -1;
+        }
+    }
+}

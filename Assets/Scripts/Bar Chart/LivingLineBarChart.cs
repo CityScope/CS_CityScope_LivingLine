9177976069b0e09b5186a3e578f8f58f3d409d73
Ryan Yan Zhang <ryanz@mit.edu>
2019-04-17 00:45:14 -0400

    WIP: bar chart

--- Assets/Scripts/Bar Chart/LivingLineBarChart.cs
+++ Assets/Scripts/Bar Chart/LivingLineBarChart.cs
@@ -4,47 +4,15 @@ using UnityEngine;
 
 public class LivingLineBarChart : MonoBehaviour
 {
-    public GameObject[] densityBar1;
-    public GameObject[] densityBar2;
-    public GameObject[] densityBar3;
-    public GameObject[] densityBar4;
-    public GameObject[] densityBar5;
-    public GameObject[] densityBar6;
+    [Range(0, 2)]
+    public int vizType = 0;
+    [Range(0, 1f)]
+    public float[] metrics;
 
-    public GameObject[] densityProfile1;
-    public GameObject[] densityProfile2;
-    public GameObject[] densityProfile3;
-    public GameObject[] densityProfile4;
-    public GameObject[] densityProfile5;
-    public GameObject[] densityProfile6;
-
-    public GameObject[] diversityBar1;
-    public GameObject[] diversityBar2;
-    public GameObject[] diversityBar3;
-    public GameObject[] diversityBar4;
-    public GameObject[] diversityBar5;
-    public GameObject[] diversityBar6;
-
-    public GameObject[] diversityProfile1;
-    public GameObject[] diversityProfile2;
-    public GameObject[] diversityProfile3;
-    public GameObject[] diversityProfile4;
-    public GameObject[] diversityProfile5;
-    public GameObject[] diversityProfile6;
-
-    public GameObject[] interactionBar1;
-    public GameObject[] interactionBar2;
-    public GameObject[] interactionBar3;
-    public GameObject[] interactionBar4;
-    public GameObject[] interactionBar5;
-    public GameObject[] interactionBar6;
-
-    public GameObject[] interactionProfile1;
-    public GameObject[] interactionProfile2;
-    public GameObject[] interactionProfile3;
-    public GameObject[] interactionProfile4;
-    public GameObject[] interactionProfile5;
-    public GameObject[] interactionProfile6;
+    public GameObject barsRoot;
+    public GameObject barSource;
+    public string barGoName = "bar profile 0";
+    public GameObject[] bars;
 
     // Update is called once per frame
     void Update()

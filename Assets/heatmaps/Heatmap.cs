2ee3c2639a8a6e0bd1eb13d954ab93abdbc57d80
Ryan Yan Zhang <ryanz@mit.edu>
2019-04-22 00:01:55 -0400

    Done: heatmap display structure, material, texture, layers

--- Assets/heatmaps/Heatmap.cs
+++ Assets/heatmaps/Heatmap.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class Heatmap : MonoBehaviour
 {
 
+    public float xPosMin;
+    public float xPosMax;
+    public float yPosMin;
+    public float yPosMax;
+    public float noise;
+
     public Vector4[] positions;
     public float[] radiuses;
     public float[] intensities;
@@ -22,8 +28,8 @@ public class Heatmap : MonoBehaviour
 
         for (int i = 0; i < positions.Length; i++)
         {
-            positions[i] = new Vector2(Random.Range(-0.4f, +0.4f), Random.Range(-0.4f, +0.4f));
-            radiuses[i] = Random.Range(0f, 0.25f);
+            positions[i] = new Vector4(Random.Range(xPosMin, xPosMax), 0f, Random.Range(yPosMin, yPosMax),0f);
+            radiuses[i] = Random.Range(0f, 100f);
             intensities[i] = Random.Range(-0.25f, 1f);
         }
     }
@@ -33,7 +39,7 @@ public class Heatmap : MonoBehaviour
         material.SetInt("_Points_Length", positions.Length);
         for (int i = 0; i < positions.Length; i++)
         {
-            positions[i] += new Vector4(Random.Range(-0.1f,+0.1f), Random.Range(-0.1f, +0.1f),0) * Time.deltaTime ;
+            positions[i] += new Vector4(Random.Range(-noise, noise), Random.Range(-noise, noise),0) * Time.deltaTime ;
             properties[i] = new Vector4(radiuses[i], intensities[i], 0, 0);
         }
 
